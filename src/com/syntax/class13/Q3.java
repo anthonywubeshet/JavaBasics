2c9d2c64c5d170822c5cb3521e2b3c50d0440df6
anthonywubeshet <anthonyw10695@gmail.com>
2020-03-27 02:56:06 -0400

    Q3 code

---
+++ src/com/syntax/class13/Q3.java
@@ -0,0 +1,29 @@
+package com.syntax.class13;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		/*
+		 * Write java to print first 10 numbers of fibonacci
+		 */
+
+	
+	int a,b,c;
+	a=0;
+	b=1;
+	
+	for(int i=1; i<=10; i++) {
+		System.out.print(a+ " ");
+		c=a+b;
+		a=b;
+		b=c;
+	}
+	
+	
+	
+	
+	
+	
+	}
+
+}
